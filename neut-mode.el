ed45dc1c82768cfc872af371febbebf23151dd8b
vekatze <vekatze@icloud.com>
2024-11-18 19:18:12 +0900

    add a keyword: void

--- neut-mode.el
+++ neut-mode.el
@@ -376,7 +376,7 @@ Intended to be used with `electric-indent-functions'."
   (define-key neut-mode-map "|" #'neut--insert-bar)
   (setq font-lock-defaults
         `(,`(("^=.*" . font-lock-doc-face)
-             (,(regexp-opt '("thread" "type" "meta" "rune" "pointer") 'symbols)
+             (,(regexp-opt '("thread" "type" "meta" "rune" "pointer" "void") 'symbols)
               . font-lock-type-face)
              (,(regexp-opt '("attach" "bind" "box" "case" "catch" "constant" "data" "default" "define" "detach" "do" "else" "else-if" "exact" "external" "foreign" "function" "if" "import" "in" "inline" "introspect" "let" "letbox" "letbox-T" "let-function" "match" "nominal" "of" "on" "pin" "quote" "resource" "tie" "try" "use" "when" "with") 'symbols)
               . font-lock-keyword-face)
